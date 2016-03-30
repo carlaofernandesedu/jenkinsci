3a1323559ea5b6441b52ec1911583524d5cfe960
Carlos Melo <carlaofernandesedu@gmail.com>
2016-03-30 13:22:57 -0300

    Alteracao para passar no teste inicial do ZIPAPPLIB e ZIPAPPWEB

--- ssetool/tools/pshell/ScriptZip.ps1
+++ ssetool/tools/pshell/ScriptZip.ps1
@@ -1,16 +1,17 @@
 ﻿function ZIP-AppLib($SourceFolder,$PubRootFolder,$Sistema,$KeepBINFiles)
 {
 #Copiar os arquivos gerados no pasta source
-Write-Host 'Copiar arquivos de' $SourceFolder 'para' $PubRootFolder
+Write-Host 'Copiando arquivos de' $SourceFolder 'para' $PubRootFolder
 Copy-Item $SourceFolder $PubRootFolder -Force -Recurse
 #Verificar restrição de arquivos definidos para o pacote 
 $FolderBINExclusao = $PubRootFolder + '\bin\*.*'
-Write-Host 'Excluir os arquivos da pasta' $FolderBINExclusao 'mantendo os arquivos' $KeepBINFiles
+Write-Host 'Excluindo os arquivos da pasta' $FolderBINExclusao 'mantendo os arquivos' $KeepBINFiles
 Remove-Item $FolderBINExclusao -Exclude $KeepBINFiles 
 #Gerar o arquivo zip 
 $ZipFile = $PubRootFolder + '\' + $Sistema + '.zip'
 Write-Host 'Gerando o arquivo ZIP' $ZipFile
-New-Item $ZipFile -ItemType File -Force 
+$FolderBINExclusao = $PubRootFolder + '\bin'
+Compress-ZIPFile $FolderBINExclusao $ZipFile
 #Remover os arquivos desnecessários
     Foreach($ItemRootExclusao in (Get-ChildItem $PubRootFolder -Recurse -File))
     { 
@@ -25,22 +26,22 @@ New-Item $ZipFile -ItemType File -Force
         Write-Host 'Removendo o diretorio' $ItemRootExclusao.FullName
         Remove-Item $ItemRootExclusao.FullName -Recurse 
     }
-
 }
 
 function ZIP-AppWeb($SourceFolder,$PubRootFolder,$Sistema,$KeepBINFiles)
 {
 #Copiar os arquivos gerados na pasta source
-Write-Host 'Copiar arquivos de' $SourceFolder 'para' $PubRootFolder
-Copy-Item $SourceFolder $PubRootFolder -Force 
+$PubStageFolder = $PubRootFolder + '\stage'
+Write-Host 'Copiando arquivos de' $SourceFolder 'para' $PubStageFolder
+Copy-Item $SourceFolder $PubStageFolder -Recurse
 #Verificar restrição de arquivos definidos para o pacote 
-$FolderBINExclusao = $PubRootFolder + '\bin\*.*'
-Write-Host 'Excluir os arquivos da pasta' $FolderBINExclusao 'mantendo os arquivos' $KeepBINFiles
+$FolderBINExclusao = $PubStageFolder + '\bin\*.*'
+Write-Host 'Excluindo os arquivos da pasta' $FolderBINExclusao 'mantendo os arquivos' $KeepBINFiles
 Remove-Item $FolderBINExclusao -Exclude $KeepBINFiles 
 #Gerar o arquivo zip 
 $ZipFile = $PubRootFolder + '\' + $Sistema + '.zip'
 Write-Host 'Gerando o arquivo ZIP' $ZipFile
-New-Item $ZipFile -ItemType File -Force 
+Compress-ZIPFile $PubStageFolder $ZipFile
 #Remover os arquivos desnecessários
     Foreach($ItemRootExclusao in (Get-ChildItem $PubRootFolder -Recurse -File))
     { 
@@ -90,9 +91,20 @@ New-Item $ZipFile -ItemType File -Force
     }
 }
 
+function Extract-ZIPFile($SourceFolderZip, $DestFolderZip)
+{
+    Add-Type -AssemblyName "system.io.compression.filesystem"
+    [io.compression.zipfile]::ExtractToDirectory((Get-ChildItem $SourceFolderZip)[0].FullName, $DestFolderZip) 
+}
+function Compress-ZIPFile($FolderToZip, $PathFileZip)
+{
+    Add-Type -AssemblyName "system.io.compression.filesystem"
+    [io.compression.zipfile]::CreateFromDirectory($FolderToZip, $PathFileZip) 
+}
 
 
 #LIB - Caso 1 Teste Sucesso ZIP-AppLib arquivos selecionados
+<#
 remove-item d:\pst -Force -Recurse
 new-item 'd:\pst\origem' -ItemType directory -Force
 new-item 'd:\pst\origem\bin' -ItemType directory -Force
@@ -102,6 +114,12 @@ new-item 'd:\pst\origem\bin\lib1.txt' -Value 'lib1'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\modulo.txt' -Value 'modulo'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\lib3.txt' -Value 'lib3'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\lib4.txt' -Value 'lib4'  -ItemType file -Force
+$pPubRootFolder = 'd:\pst\destino'
+$pSourceFolder = 'd:\pst\origem\bin'
+$pSistema = 'sistema'
+$pKeepBINFiles = @('modulo.txt')
+ZIP-AppLib $pSourceFolder $pPubRootFolder $pSistema $pKeepBINFiles
+#>
 
 #LIB - Caso 2 Teste Sucesso ZIP-AppLib arquivos todos
 <#
@@ -114,11 +132,16 @@ new-item 'd:\pst\origem\bin\lib1.txt' -Value 'lib1'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\modulo.txt' -Value 'modulo'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\lib3.txt' -Value 'lib3'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\lib4.txt' -Value 'lib4'  -ItemType file -Force
+$pPubRootFolder = 'd:\pst\destino'
+$pSourceFolder = 'd:\pst\origem\bin'
+$pSistema = 'sistema'
+$pKeepBINFiles = @('modulo.txt')
+ZIP-AppLib $pSourceFolder $pPubRootFolder $pSistema $pKeepBINFiles
 #>
 
 
-#WEB - Caso 2 Teste Sucesso ZIP-AppWeb
-<#
+#WEB - Caso 3 Teste Sucesso ZIP-AppWeb
+remove-item d:\pst -Force -Recurse
 new-item 'd:\pst\origem' -ItemType directory -Force
 new-item 'd:\pst\origem\subpasta' -ItemType directory -Force
 new-item 'd:\pst\origem\bin' -ItemType directory -Force
@@ -130,12 +153,10 @@ new-item 'd:\pst\origem\subpasta\web3.aspx' -Value 'webpage3'  -ItemType file -F
 new-item 'd:\pst\origem\bin\modulo.txt' -Value 'modulo'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\libweb2.txt' -Value 'libweb2'  -ItemType file -Force
 new-item 'd:\pst\origem\bin\libweb3.txt' -Value 'libweb3'  -ItemType file -Force
-#>
-
 $pPubRootFolder = 'd:\pst\destino'
-$pSourceFolder = 'd:\pst\origem\bin'
+$pSourceFolder = 'd:\pst\origem'
 $pSistema = 'sistema'
 $pKeepBINFiles = @('modulo.txt')
-
-ZIP-AppLib $pSourceFolder $pPubRootFolder $pSistema $pKeepBINFiles
+#ZIP-AppLib $pSourceFolder $pPubRootFolder $pSistema $pKeepBINFiles
+ZIP-AppWeb $pSourceFolder $pPubRootFolder $pSistema $pKeepBINFiles
 
